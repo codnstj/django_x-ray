bfdad03e34bf1eee6bc481bd8acf7bb294bb5e7e
codnstj <mail@chaewoon.me>
2023-09-21 14:38:39 +0900

    segment & subsegment

--- xray/views.py
+++ xray/views.py
@@ -3,7 +3,12 @@ from rest_framework import viewsets
 from xray.serializer import UserSerializer
 from django.contrib.auth.models import User
 
+segment = xray_recorder.begin_segment('ViewSets')
+
 class UserViewSet(viewsets.ModelViewSet):
+    subsegment = xray_recorder.begin_subsegment('UserViewSet')
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+xray_recorder.end_subsegment()
+xray_recorder.end_segment()
